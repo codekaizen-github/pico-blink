36df7934f59a1448aa1ee03fbc5dc4df0c17b251
Missie Dawes <missiedawes@gmail.com>
2025-05-29 20:05:35 -0400

    Test with pin 13

--- main.py
+++ main.py
@@ -1,13 +1,16 @@
 from machine import Pin
 from utime import sleep
 
-pin = Pin("LED", Pin.OUT)
+pin = Pin(13, Pin.OUT)
+# pin.on()  # Turn the pin on to start with
+# pin.value(0)  # Ensure the pin is off initially
 
 print("LED starts flashing...")
-for i in range(20):
+for i in range(25):
     try:
+        print(f"Flashing {i + 1}...")
         pin.toggle()
-        sleep(.25)  # sleep 1sec
+        sleep(.5)  # sleep 1sec
     except KeyboardInterrupt:
         break
 pin.off()
