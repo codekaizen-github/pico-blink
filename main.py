e1815e42162b21276b19c59f087567f58d2e6776
Andrew Dawes <andrewjamesdawes@gmail.com>
2025-06-29 10:33:24 -0400

    Refactor LED control logic to use a flash pattern instead of button state

--- main.py
+++ main.py
@@ -3,8 +3,12 @@ led_pin = Pin(15, mode=Pin.OUT)
 button_pin = Pin(14, mode=Pin.IN, pull=Pin.PULL_UP)
 BUTTON_ON = 0
 BUTTON_OFF = 1
-while True:
-    if (BUTTON_ON == button_pin.value()):
-        led_pin.on()
-    else:
-        led_pin.off()
+
+flash_pattern = [50000, 40000, 20000, 30000, 700000]
+
+if True:
+    for cycles in flash_pattern:
+        led_pin.toggle()
+        for _ in range(cycles):
+            pass
+    led_pin.toggle()
